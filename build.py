5fedfc66c552af9883f5c80bcf4b969f618c6e96
Ryan Houck <ryan@levelskies.com>
2014-10-18 13:49:25 -0600

    more dev

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ from math import exp, log, sqrt
 from sklearn.linear_model import SGDClassifier
 import numpy as np
  
-D = 2 ** 20   # number of weights use for learning
-alpha = .1    # learning rate for sgd optimization
+D = 2 ** 10  # number of weights use for learning
 
 def logloss(p, y):
     p = max(min(p, 1. - 10e-12), 10e-12)
+    #print p
     return -log(p) if y == 1. else -log(1. - p)
 
 def get_x(csv_row, D):
@@ -37,20 +37,21 @@ def update_w(w, n, x, p, y):
 
     return w, n
 
+
+train = 'source/train_small.txt'
+reader = csv.reader( open( train, 'r' ), delimiter = '\t' )
+
+"""
+alpha = .1    # learning rate for sgd optimization
 w = [0.] * D  # weights
 n = [0.] * D  # number of times we've encountered a feature
 
-
-clf = SGDClassifier(loss='modified_huber',)
-#clf.partial_fit(np.zeros(D), np.asarray([0.,]), classes=np.asarray([0.,1.]))
-
 loss = 0.
-train = 'source/train_small.txt'
-reader = csv.reader( open( train, 'r' ), delimiter = '\t' )
+accuracy = 0.
 for t, row in enumerate(reader):
-    if t > 100: 
-        break
-
+    #if t > 10: 
+    #    break
+    
     y = 1. if row[0] == '1' else 0.
 
     del row[0]  # can't let the model peek the answer
@@ -58,36 +59,80 @@ for t, row in enumerate(reader):
     # main training procedure
     # step 1, get the hashed features
     x = get_x(row, D)
-    """
+    
     # step 2, get prediction
     p = get_p(x, w)
 
+    # estimate classification
+    pred = 1. if p >= 0.5 else 0.
+    if pred == y:
+        accuracy += 1.
+
     # for progress validation, useless for learning our model
     loss += logloss(p, y)
-    if t % 10 == 0 and t > 1:
-        print('%s\tencountered: %d\tcurrent logloss: %f' % (
-            datetime.now(), t, loss/t))
+    if t % 100 == 0 and t > 1:
+        print('%s\tencountered: %d\tcurrent logloss: %f\tcurrent score: %f' % (datetime.now(), t, loss/t, accuracy/t))
+        #print('%s\tencountered: %d\tcurrent logloss: %f' % (
+        #    datetime.now(), t, loss/t))
 
     # step 3, update model with answer
     w, n = update_w(w, n, x, p, y)
-    """
+"""
+
+def format_x(csv_row, D):
+    #x = [0]  # 0 is the index of the bias term
+    x = []
+    for count, value in enumerate(csv_row):
+        index = int(value + str(count), 16) % D  # weakest hash ever ;)
+        x.append(index)
+    return x  # x contains indices of features that have a value of 1
+
+
+clf = SGDClassifier(loss='log', learning_rate='invscaling')
+clf.partial_fit(np.zeros(D), np.asarray([0.,]), classes=np.asarray([0.,1.]))
+blank_row = np.zeros(D)
+loss = 0.
+accuracy = 0.
+for t, row in enumerate(reader):
+    y = 1. if row[0] == '1' else 0.
+
+    del row[0]  # can't let the model peek the answer
+    
+    # main training procedure
+    # step 1, get the hashed features
+    x = format_x(row, D)
+
     #SGDClassifier
-    long_row = np.zeros(D)
     # add categorial values
+    
+    long_row = blank_row
     long_row[x] = 1.
     
-    # fit model for one row
-    clf.partial_fit(x, np.asarray([y]), classes=np.asarray([0.,1.]))
-
-    # estimate classification
-    pred = clf.predict(x)
-    #print pred
+    # figure best way to deal with sparce data
 
     # estimate liklihood classification is 1
-    pred_proba = clf.predict_proba(x)
-    print pred_proba
-    """
-    loss += logloss(pred_proba[1], y)
-    if t % 10 == 0 and t > 1:
-        print('%s\tencountered: %d\tcurrent logloss: %f' % (datetime.now(), t, loss/t))
-    """
+    p = clf.predict_proba(long_row)[0][1]
+    loss += logloss(p, y)
+    
+    # estimate classification
+    pred = 1. if p >= 0.5 else 0.
+    if pred == y:
+        accuracy += 1.
+
+    if t % 100 == 0 and t > 1:
+        print('%s\tencountered: %d\tcurrent logloss: %f\tcurrent score: %f' % (datetime.now(), t, loss/t, accuracy/t))
+        #print clf.coef_
+    
+    # ensure predicted probability is never 1 or 0
+
+    # add separate out integer features
+
+
+    # scale integer featuers - rolling average / standard deviations
+    #z = (x - mean) / std_dev
+
+
+    # fit model for one row
+    clf.partial_fit(long_row, np.asarray([y]))
+   
+    
